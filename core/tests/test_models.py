4b3a17c60c36736d1739749eef7d410a563e942d
lea.arturi <lea.arturi@gmail.com>
2021-09-08 16:02:12 -0300

    first commit

---
+++ core/tests/test_models.py
@@ -0,0 +1,34 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTest(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """ Probar creando un nuevo usuario con un email valido """
+
+        email = 'test@test.com'
+        password = '123456'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalize(self):
+        """ Testea email para nuevo user """
+
+        email = 'test@COREO.COM'
+        password = '123456'
+
+        user = get_user_model().objects.create_user(email, password)
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """ Nuevo usuario envia un email invalido """
+
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, '123456')
